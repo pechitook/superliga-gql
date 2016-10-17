9b1b2c5b6421dc8aa9263a3b47dd3bbedd4e5188
Pablo Chiappetti <pchiappetti@gmail.com>
2016-10-17 11:17:23 -0300

    comply with linter

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-import express from 'express';
-import { apolloExpress, graphiqlExpress } from 'apollo-server';
-import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
-import bodyParser from 'body-parser';
-import Schema from './src/schema';
-import Resolvers from './src/resolvers';
+import express from 'express'
+import { apolloExpress, graphiqlExpress } from 'apollo-server'
+import { makeExecutableSchema } from 'graphql-tools'
+import bodyParser from 'body-parser'
+import Schema from './src/schema'
+import Resolvers from './src/resolvers'
 
-const GRAPHQL_PORT = process.env.PORT || 4000;
+const GRAPHQL_PORT = process.env.PORT || 4000
 
-const graphQLServer = express();
+const graphQLServer = express()
 
 const executableSchema = makeExecutableSchema({
   typeDefs: Schema,
   resolvers: Resolvers,
-});
+})
 
 graphQLServer.use('/graphql', bodyParser.json(), apolloExpress({
   schema: executableSchema,
-}));
+}))
 
 graphQLServer.use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql',
-}));
+}))
 
+/* eslint-disable no-console */
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`
-));
+))
