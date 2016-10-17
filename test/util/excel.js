1ec4e3b4a822c60e5061af3d022ab54fb498c746
Pablo Chiappetti <pchiappetti@gmail.com>
2016-10-17 10:40:11 -0300

    test excel util helpers and fix getCellColumn

---
+++ test/util/excel.js
@@ -0,0 +1,20 @@
+import { getCellNumber, getCellColumn } from 'util/excel'
+import expect from 'expect'
+
+describe('getCellNumber', () => {
+  it('returns the number of a cell', () => {
+    expect(getCellNumber('A3')).toBe(3)
+    expect(getCellNumber('A33')).toBe(33)
+  })
+  it('returns NaN when invalid cell is passed', () => {
+    expect(getCellNumber('ABC')).toEqual(NaN)
+  })
+})
+describe('getCellColumn', () => {
+  it('returns the column of a cell', () => {
+    expect(getCellColumn('A3')).toBe('A')
+  })
+  it('works with double-letter columns', () => {
+    expect(getCellColumn('AC33')).toBe('AC')
+  })
+})
